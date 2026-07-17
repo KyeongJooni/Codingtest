0287ca091395428839fd4ee44c10af7671c04c8a
Lee KyeongJoon <rudwns0224@naver.com>
2026-07-17 17:00:18 +0900

    [level 0] Title: 조건 문자열, Time: 0.07 ms, Memory: 44 MB -BaekjoonHub

---
+++ 프로그래머스/0/181934. 조건 문자열/조건 문자열.js
@@ -0,0 +1,12 @@
+function solution(ineq, eq, n, m) {
+  if(ineq === ">" && eq === "="){
+      return n>=m ? 1 : 0;
+  }
+  if(ineq === "<" && eq === "="){
+      return n<=m ? 1 : 0;
+  }  
+  if(ineq === ">"){
+      return n>m ? 1 : 0;
+  }
+  return n>m ? 0 : 1;
+}
